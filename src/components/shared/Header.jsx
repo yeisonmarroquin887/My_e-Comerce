e10da3a9e9052afed733a43e8a944bf238b03429
Yeison Andrés Marroquín Bernal <121591987+yeisonmarroquin887@users.noreply.github.com>
2023-07-26 11:42:58 -0500

    My Sena

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './style/header.css'
 
 const Header = () => {
   return (
-    <header>
-        <h1><Link to='/'>E-Commerce </Link></h1>
-        <nav>
-            <ul>
-                <li> <Link to='/login'>Login</Link> </li>
-                <li> <Link to='/register'>Register</Link> </li>
-                <li> <Link to='/'>Purchases</Link> </li>
-                <li> <Link to='/cart'>Cart</Link> </li>
+    <header className='Header'>
+        <h1 className='Header_h1'><Link to='/'>E-Commerce </Link></h1>
+        <nav className='Header_nav'>
+            <ul className='Header_ul'>
+                <li className='Header_li'> <Link to='/login'>Login</Link> </li>
+                <li className='Header_li'> <Link to='/register'>Register</Link> </li>
+                <li className='Header_li'> <Link to='/'>Purchases</Link> </li>
+                <li className='Header_li'> <Link to='/cart'>Cart</Link> </li>
             </ul>
         </nav>
     </header>
