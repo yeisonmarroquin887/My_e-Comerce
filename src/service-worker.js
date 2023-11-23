a646d5c6e7d2b78a6d6d0d5f58321e86a707e95e
Yeison Andrés Marroquín Bernal <121591987+yeisonmarroquin887@users.noreply.github.com>
2023-11-22 19:58:49 -0500

    My Api

---
+++ src/service-worker.js
@@ -0,0 +1,24 @@
+// service-worker.js
+
+const CACHE_NAME = 'mi-cache';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/icon-192x192.png',
+        // Agrega otras rutas de tus recursos aquí
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
